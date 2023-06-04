973d6c7915e5ca4bc1d3fb50873470d429c564ba
EV7H4N4514 <66260859+EV7H4N4514@users.noreply.github.com>
2023-06-04 11:06:36 -0300

    Add files via upload

---
+++ assets/js/tickets.js
@@ -0,0 +1,52 @@
+//categoría
+identificarAsistente = () => {
+    const persona = document.getElementById('categoria');
+    const asistente = persona.value;
+        return asistente;
+}
+
+// cantidad
+identificarCantidad = () => {
+const apuntadorAsistentes = document.getElementById('cantidad')
+const valorAsistentes = apuntadorAsistentes.value
+const numeroAsistentes = toNumber(valorAsistentes)
+    return numeroAsistentes
+}
+
+function toNumber(valor) {
+    const apuntadorValor = Number(valor);
+    return apuntadorValor;
+}
+
+function calcularDescuento() {
+    let total;
+    let unaCantidad = identificarCantidad()
+    let unaCategoria = identificarAsistente()
+
+    switch(unaCantidad, unaCategoria) {
+        case 'estudiante':
+            total = unaCantidad * (200 * 0.2);
+            break;
+        case 'trainee':
+            total = unaCantidad * (200 * 0.5);
+            break;
+        case 'junior':
+            total = unaCantidad * (200 * 0.85);
+            break;
+        default:
+            break;
+    }
+    //apuntador al precio
+    const apuntarPrecio = document.getElementById('totalAPagar');
+    apuntarPrecio.innerHTML = total
+}
+
+//borrar
+const borrarFormulario = () => {
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    categoria.value = "";
+    cantidad.value = "";
+    totalAPagar.value = "";
+}
